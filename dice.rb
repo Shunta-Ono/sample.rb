28c2e4c22e4a26823f8ea802e7ca8823bf5237da
onoshunta <nisikoubasball.a@gmail.com>
2019-12-17 01:44:15 +0900

    [Finish]2-8

---
+++ dice.rb
@@ -0,0 +1,9 @@
+dice = 0
+while dice != 6 do
+  dice = rand(1..6)
+  puts dice
+end
+
+#diceを0で初期化
+#6でない間繰り返す
+#while文は指定した条件によって繰り返しの有無が判断される
---
+++ each.rb
@@ -0,0 +1,6 @@
+{"apple"=>130,"strawberry"=>190,"orange"=>100}.each do |fruit,price|
+  puts "#{fruit}は#{price}円です"
+end
+#(範囲,ハッシュ,配列などを指定).each do |変数|
+#実行する処理
+#end
